596753561058f37ac1bf115978cf175f05f6c1f6
QuantumTCode <neil.nitin.de@Gmail.com>
2018-04-26 01:50:38 -0400

    Update retrain.py

--- scripts/retrain.py
+++ scripts/retrain.py
@@ -419,7 +419,7 @@ def get_or_create_bottleneck(sess, image_lists, label_name, index, image_dir,
                            image_dir, category, sess, jpeg_data_tensor,
                            decoded_image_tensor, resized_input_tensor,
                            bottleneck_tensor)
-
+  bottleneck_path = (bottleneck_path[:200] + '..') if len(bottleneck_path) > 75 else bottleneck_path
   with open(bottleneck_path, 'r') as bottleneck_file:
     bottleneck_string = bottleneck_file.read()
   did_hit_error = False
